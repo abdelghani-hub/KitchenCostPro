b9b2426c3c9a7e51c86c1df6b46cc522f9893d74
abdelghani <aaittamghart9@gmail.com>
2024-09-24 09:45:50 +0100

    Improve project details reading

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -96,13 +96,13 @@ public class Project {
 
     public String toString() {
         return
-                "\n\t------------------" +
-                "\n\tName :" + name +
-                "\n\tProfitMargin :" + profitMargin +
-                "\n\tTotalCost :" + totalCost +
-                "\n\tStatus :" + status +
-                "\n\tArea :" + area +
-                "\n\tTVA :" + TVA +
+                "\n\t----------------------------------------------" +
+                "\n\tName                : " + name +
+                "\n\tProfitMargin        : " + profitMargin +
+                "\n\tTotalCost           : " + totalCost +
+                "\n\tStatus              : " + status +
+                "\n\tArea                : " + area +
+                "\n\tTVA                 : " + TVA +
                 "\n\tTotalCostWithMargin : " + ConsoleUI.ORANGE + ConsoleUI.formatDouble(getTotalCostWithMargin()) + ConsoleUI.RESET + " €\n";
     }
 }
